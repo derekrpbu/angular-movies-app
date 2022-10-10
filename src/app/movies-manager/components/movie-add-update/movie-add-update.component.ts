368fc1077409a9c37d31605cee15df7409131619
Derek Robert <Derek.Robert@kyndryl.com>
2022-10-10 13:13:31 -0600

    initial setup

---
+++ src/app/movies-manager/components/movie-add-update/movie-add-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-add-update',
+  templateUrl: './movie-add-update.component.html',
+  styleUrls: ['./movie-add-update.component.css']
+})
+export class MovieAddUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
